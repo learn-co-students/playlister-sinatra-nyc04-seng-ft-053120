b6f73e2ac11c9570f50325108eef80d9b74b2798
415CA <chrisallen415@gmail.com>
2020-07-15 14:43:52 -0400

    seeded data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 5) do
+ActiveRecord::Schema.define(version: 2020_07_15_180627) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
